26bc7052b807fdd880cd1c8995a7665b94868a09
Michael Connor Buchan <mikey@blindcomputing.org>
2025-05-08 22:41:13 +0000

    build: Run refined bootstrap script before starting server

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu
+
 # Define the paths to check and their types (file or directory)
 declare -A paths=(
     ["./database.sqlite3"]="file"
@@ -11,24 +13,13 @@ declare -A paths=(
 for path in "${!paths[@]}"; do
     if [ ! -e "$path" ]; then
         if [ "${paths[$path]}" = "file" ]; then
-            echo "Creating file: $path..."
             touch "$path"
+            echo "touch: Created file $path"
         elif [ "${paths[$path]}" = "directory" ]; then
-            echo "Creating directory: $path..."
-            mkdir -p "$path"
+            mkdir -vp "$path"
         else
             echo "Invalid type specified for $path"
             exit 1
         fi
-        if [ $? -eq 0 ]; then
-            echo "Successfully created $path"
-        else
-            echo "Failed to create $path"
-            exit 1
-        fi
-    else
-        echo "$path already exists"
     fi
 done
-
-echo "All required paths are ready"
